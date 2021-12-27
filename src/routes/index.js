b3a61d1feb38b42824f0eae34688316ed8f486fb
shuhaibp7 <pshuhaib077@gmail.com>
2021-12-26 21:39:01 -0800

    customer login

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import PartnerLogin from "../pages/partner/login";
+import CustomerLogin from "../pages/customer/login";
 import Deals from "../pages/partner/deals";
 
 export default function AppRoutes() {
@@ -9,7 +10,7 @@ export default function AppRoutes() {
 
   return session == "" ? (
     <Routes>
-      <Route path="/" element={<PartnerLogin />} />
+      <Route path="/" element={<CustomerLogin />} />
     </Routes>
   ) : (
     <Routes>
--- src/urls/apiUrls.js
+++ src/urls/apiUrls.js
@@ -1,8 +1,10 @@
-const BASE_URL = "http://localhost:8000/controllers/";
+const BASE_URL = "http://localhost/JRMDistributionServer/controllers/";
 
 export const PARTNER_URL = {
   LOGIN: BASE_URL + "partner/login.php",
   GET_DEALS: BASE_URL + "deal/getDeals.php",
 };
 
-export const CUSTOMER_URL = {};
+export const CUSTOMER_URL = {
+  LOGIN: BASE_URL + "customerLogin.php",
+};
